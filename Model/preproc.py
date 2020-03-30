1b122d47f60a671dae15f35c68aac9d0928d72b1
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-03-30 23:59:02 +0530

    predict result

---
+++ Model/preproc.py
@@ -0,0 +1,65 @@
+import pickle
+import random
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import cv2
+
+DATADIR = '/home/shrinidhi/Desktop/sem6/Waste-Classification-App/Model/TRAIN'
+CATEGORIES = ['O', 'R']
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category) # path to O or R dir
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap='gray')
+        # plt.show()
+        break
+    break
+
+print(img_array.shape)
+
+IMG_SIZE = 70                                 
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+# plt.show()
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category) # path to O or R dir
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+create_training_data()
+
+print(len(training_data))
+
+random.shuffle(training_data)
+
+for sample in training_data[:10]:
+    print(sample[1])
+
+X = []
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1) # 1: because it is grayscale
+
+pickle_out = open('X.pickle', 'wb')
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open('y.pickle', 'wb')
+pickle.dump(y, pickle_out)
+pickle_out.close()
