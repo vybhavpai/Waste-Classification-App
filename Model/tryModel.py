5df1ece0fc595f486486cbcf9d68bbc96b28a71b
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-05-12 15:14:10 +0530

    trying stuff

---
+++ Model/tryModel.py
@@ -0,0 +1,66 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+# from keras.optimizers import RMSprop
+import tensorflow as tf
+import pickle
+import time
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [0]
+layer_sizes = [64]
+conv_layers = [3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(
+                conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+                model.add(Dropout(0.5))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            # opt = RMSprop()
+            opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer=opt,
+                          metrics=['accuracy'])
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
+
+model.save('64x3-CNN.model')
